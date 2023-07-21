a183e02bf4da5a8d03f63805f79b0b9b896ec8c8
VickyGhoderao <vickyghoderao0012@gmail.com>
2023-07-21 17:07:32 +0530

    program is updated input from user

---
+++ addition3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;int b;
+    printf("Enter a first number:");
+    scanf("%d",&a);
+    printf("Enter a second number:");
+    scanf("%d",&b);
+    printf("addition of two number is:%d",a+b);
+
+}
